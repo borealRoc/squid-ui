083ebc7c6c834acb67f26c88f4cb967464345486
xusp <xspstudyemail@163.com>
2022-10-24 15:31:19 +0800

    test: 测试组件库的按需引入功能

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 // import SquidUI from "./entry";
-import SquidUI from "../dist/squid-ui.mjs";
 
-createApp(App).use(SquidUI).mount("#app");
+// 完整引入
+// import SquidUI from "../dist/squid-ui.mjs"
+// createApp(App).use(SquidUI).mount("#app");
+
+// 按需引入
+import { TSXButton, VueButton } from "../dist/squid-ui.mjs";
+createApp(App)
+.component(TSXButton.name, TSXButton)
+.component(VueButton.name, VueButton)
+.mount("#app");
+
