08dc9c6e9b28704aebea19890d828584d146267a
xusp <xspstudyemail@163.com>
2022-10-31 16:17:12 +0800

    build: 新增组件库打包出sourceMap

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import vue from "@vitejs/plugin-vue";
 import vueJSX from "@vitejs/plugin-vue-jsx";
 import Unocss from "./src/config/unocss";
 
+// 由于 Vite 的构建是通过 rollup 完成的，所以 rollup 中的一些配置通过这个属性传递给 rollup
 const rollupOptions = {
+  // 将该模块保留在 bundle 之外，比如在数组中添加了 vue ，就是为了不让 vue 打包到组件库中
   external: ["vue"],
+  // 这个配置用于 umd/iffe 包中，意思是全局中的某个模块在组件库中叫什么名字
   output: {
     globals: {
       vue: "Vue",
@@ -18,13 +21,15 @@ export default defineConfig({
   plugins: [vue(), vueJSX(), Unocss()],
   build: {
     rollupOptions,
-    minify: `terser`, // boolean | 'terser' | 'esbuild'
+    minify: `terser`,   // 压缩混淆选项：boolean | 'terser' | 'esbuild'
     cssCodeSplit: true, // Build 时单独导出 CSS
+    sourcemap: true,    // 导出 SourceMap
+    reportCompressedSize: true,  // 生成压缩大小报告
     lib: {
       entry: "./src/entry.ts",
-      name: "SquidUI",
-      fileName: "squid-ui",
-      formats: ["es", "umd", "iife"],
+      name: "SquidUI",      // 生成包的名字，在 iife/umd 包，同一页上的其他脚本可以访问它
+      fileName: "squid-ui", // 是文件名，其实只是一个输出文件名的前缀，默认情况下会和模块类型配合组成最终的文件名
+      formats: ["es", "umd", "iife"], // 导出模块类型
     },
   },
   test: {
